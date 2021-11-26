b54ae7e40690d017044a55718443045588e9c300
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-27 01:08:49 +0530

    New code

---
+++ summation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout << "Enter a value: ";
+    cin >> a;
+    cout << "Enter b value: ";
+    cin >> b;
+    c = a + b;
+    cout << "The summation is: "<< c << endl;
+    return 0;
+}
