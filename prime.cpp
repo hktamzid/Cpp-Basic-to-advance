e03a5250267123caca81a5768660ea8ba4b1ce49
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-27 11:00:34 +0530

    new code

---
+++ prime.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter a positive number: ";
+    cin >> num;
+    
+    return 0;
+}
---
+++ string.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name [14]= "Tamzid Mahmud";
+    cout << "My name is "<< name;
+    return 0;
+}
