24383eb52797f183c324e848c5c504fcf01e7ff3
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-29 22:03:32 +0530

    New text added

--- formatting.cpp
+++ formatting.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<conio.h>
+#include<iomanip> 
 using namespace std;
 int main()
 {
@@ -9,18 +10,21 @@ int main()
     cin >> num1 >> num2 ;
 
     cout << showpoint;
+    cout << fixed;
+    cout << setprecision(2);
 
     float sum = num1 + num2;
-    cout << "Summation is: "<< sum <<endl ;
+    cout << setw(20) <<"Summation is: "<< sum <<endl ;
    
     float sub = num1 - num2;
-    cout << "Subtraction is: " << sub << endl ;
-  
+    cout << setw(20) <<"Subtraction is: " << sub << endl ;
+    
+    cout << noshowpoint;
     float mul = num1 * num2;
-    cout << "Multiplication is: " << mul << endl ;
+    cout << setw(20) <<"Multiplication is: " << mul << endl ;
 
     float div = num1 / num2;
-    cout << "Division is: " << div << endl ;
+    cout << setw(20) <<"Division is: " << div << endl ;
 
     //float mod = num1 % num2;
    // cout << "Reminder is: " <<mod ;
