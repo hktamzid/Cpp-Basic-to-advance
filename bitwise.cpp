98f54701844297a703f8f63bd9df2bdf8e69eb00
hktamzid <20mh1a0536@acoe.edu.in>
2021-11-30 15:53:50 +0530

    New code

---
+++ bitwise.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    int a=32, b=12,c;
+
+    c = a & b;
+    cout << "C value is: "<< c <<endl;
+
+    c = a | b;
+    cout << "C value is: "<< c <<endl;
+
+    c = a ^ b;
+    cout << "C value is: "<< c <<endl;
+
+    //c = a ~ 2;
+    //cout << "C value is: "<< c <<endl;
+
+    c = b << 2; 
+    cout << "C value is: "<< c <<endl;
+
+    c = a >> 2;
+    cout << "C value is: "<< c <<endl;
+
+    getch();
+    return 0;
+}
