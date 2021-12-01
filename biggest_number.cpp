3ae4859465b07159d1ea97b3f9685566863e488d
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-01 21:33:00 +0530

    new code

---
+++ biggest_number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout << "Enter two integer numbers: ";
+    cin >> a >> b ;
+
+    if (a>b)
+    {
+        cout <<"Biggest number is: "<< a <<endl;
+    }
+    else
+    {
+        cout << "Biggest number is: "<< b <<endl;
+    }
+    
+
+    return 0;
+}
