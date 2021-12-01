971659cc08f21910f5112a6ba3a7294013f174a0
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-01 11:52:02 +0530

    New code

---
+++ specialoperator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    float b;
+    double c;
+    char ch;
+    char str[20];
+
+    int v = sizeof(a);
+    cout << v;
+
+    getch();
+    return 0;
+}
