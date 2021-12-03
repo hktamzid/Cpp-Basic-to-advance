239eef6c02b9a5d6635a4feb416bdfc0e879322e
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-03 23:11:47 +0530

    edited

--- leap_year.cpp
+++ leap_year.cpp
@@ -7,14 +7,14 @@ int main ()
     cout << "Enter a year: ";
     cin >> year;
 
-    if (year%4==0 && year%100!=0)
+    if ((year%4==0 && year%100!=0) || (year%400==0))
     {
         cout << "Leap year";
     }
-    else if(year%400==0)
-    {
-        cout << "Leap year";
-    }
+    //else if(year%400==0)
+    //{
+    //    cout << "Leap year";
+    //}
     else
     {
         cout << "Not Leap year";
