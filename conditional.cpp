3be286354d9cb89ace826a1c16879b89bbbb0184
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-05 21:57:43 +0530

    new code

---
+++ conditional.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1=20;
+    int num2=30;
+    int max;
+
+    max = (num1>num2) ? num1 : num2;
+    cout << max <<" is max number";
+
+    return 0;
+}
