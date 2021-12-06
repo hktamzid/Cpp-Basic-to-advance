3d2b3c6eecb010ec8b682111f8880b6af084e008
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-07 00:07:55 +0530

    new code

---
+++ forloop1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+   for (int i=0; i <=5; i++)
+   {
+       cout << "Tamzid Mahmud" << endl;
+   }
+   
+    return 0;
+}
