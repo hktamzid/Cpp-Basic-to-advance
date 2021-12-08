bb1aaa4edf0018be51e5aeb151d0bc37cecd6ae8
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-08 20:36:44 +0530

    new code

---
+++ dowhile.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout << "Bangladesh: " << i << endl;
+        i++;
+
+    } while (i<=50);
+
+    return 0;
+}
