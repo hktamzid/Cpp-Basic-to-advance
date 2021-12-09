96e3f810231e7b0b9c082c53dd15bd40e0d8970e
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-09 20:29:18 +0530

    new code

---
+++ matrix22.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int a[2][2],i,j;
+    printf("Enter the input 2*2 matrix: \n");
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 2; j++)
+        scanf("%d\t",&a[i][j]);
+    }
+    printf("\nDisplay the matrix value:\t");
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 2; j++)
+        printf("%d\t",a[i][j]);
+    }
+    printf("\nThis is a 2*2 matrix");
+
+    return 0;
+}
