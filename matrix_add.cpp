e4bb2f675dc0544fac8f7b953f65fd38130da8e6
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-09 20:53:37 +0530

    new code

---
+++ matrix_add.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    int a[2][3], b[2][3], c[2][3];
+    printf("Read the input for matrix a: \n");
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        
+            scanf("%d\t",&a[i][j]);
+        
+    }
+    printf("\nRead the input for matrix b: \t");
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        
+            scanf("%d\t",&b[i][j]);
+        
+    }
+    
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        
+            c[i][j] = a[i][j] + b[i][j];
+        
+    }
+    printf("The sum of 2 matrix: \n");
+    for ( i = 0; i < 2; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        
+            printf("%5d\t",c[i][j]);
+        printf("\n");
+        
+    }
+
+    return 0;
+}
