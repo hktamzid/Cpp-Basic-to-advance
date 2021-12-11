0bde4cdd7cd08c7d28c72940375c5ab8862117a8
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-11 23:31:58 +0530

    new code

--- break.cpp
+++ break.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
     int i;
-    for(i=1; i<=100; i++)
+    for(i=1; i<=100; i+=2)
     {
         if(i==10)
         {
             break;
         }
+        cout << i << endl;
     }
-    cout << i << endl;
 
     return 0;
 }
