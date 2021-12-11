6e6e3bfce7fa057b8336f8ffafc6925c5423c5a6
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-11 23:27:14 +0530

    new code

---
+++ break.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=1; i<=100; i++)
+    {
+        if(i==10)
+        {
+            break;
+        }
+    }
+    cout << i << endl;
+
+    return 0;
+}
