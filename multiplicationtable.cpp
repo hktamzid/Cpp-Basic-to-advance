9e5d77d0f3d45f9f6376182d017812bac8b5b8bc
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-12 11:52:17 +0530

    new code

---
+++ multiplicationtable.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+    int num,table;
+    cout << "Enter the number that's table you want: ";
+    cin >> num;
+
+    cout << "The Table is:"<< endl;
+    for(int i=1; i<=10; i++)
+    {
+        table = num * i;
+        cout << num << " + " << i << " = "<< table << endl;
+    }
+
+    return 0;
+}
