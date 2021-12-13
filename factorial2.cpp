3ee3d18e75370976126e46fd33edcfd385fa1e04
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-13 22:28:08 +0530

    new code

--- factorial2.cpp
+++ factorial2.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main()
 {
-    int n,fact = 1;
+    int n, fact=1;
     cout << "Enter any number: ";
     cin >> n;
 
-    for( int i = 1; i <= n; i++)
+    for(int i=1; i<=n; i++)
     {
         fact = fact * i;
     }
-    cout << "Factorial is: " << fact<< endl;
+    cout << fact;
 
     return 0;
 }
