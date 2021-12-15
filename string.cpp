ae10e6a16f9235793ed0dfef8ef2c4c096f23e78
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-15 23:32:33 +0530

    new code

--- string.cpp
+++ string.cpp
@@ -3,8 +3,12 @@ using namespace std;
 int main()
 {
     char name [20];
+    int roll;
     cout << "Enter your name: ";
     cin >> name;
-    cout << "My name is "<< name << endl;
+    cout << "Enter your roll: ";
+    cin >> roll;
+    cout << "My name is: "<< name << endl;
+    cout << "Roll number is: "<< roll << endl;
     return 0;
 }
