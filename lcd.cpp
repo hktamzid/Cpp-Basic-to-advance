223692bdf6ac6ee59ae3fdd699e6ef96c21d3599
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-16 23:50:13 +0530

    new code

--- lcd.cpp
+++ lcd.cpp
@@ -5,9 +5,23 @@ using namespace std;
 int main()
 {
     int num1,num2,n1,n2,gcd,lcd,rem;
+    cout << "Enter two numbers: " << endl;
+    cin >> num1 >> num2;
+
     n1 = num1;
     n2 = num2;
-    cout >> "Enter two numbers: ";
+
+    while(n2!=0)
+    {
+        rem = n1 % num2;
+        n1 = n2;
+        n1 = rem;
+    }
+    gcd = n1;
+    cout << "GCD is: " << gcd << endl;
+
+    lcd = num1 * num2 / gcd;
+    cout << "LCD is: " << lcd << endl;
 
     return 0;
 }
