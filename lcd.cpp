15461426e286dfbad3fb0661a25071fd08888438
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-16 23:43:54 +0530

    new add

---
+++ lcd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int num1,num2,n1,n2,gcd,lcd,rem;
+    n1 = num1;
+    n2 = num2;
+    cout >> "Enter two numbers: ";
+
+    return 0;
+}
