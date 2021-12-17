830390644642d2d4aeec93604758dde35f101899
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-17 23:21:56 +0530

    new code

---
+++ sumofdigit.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int sum =0, temp, num, rem;
+    cout << "Enter any positive number: ";
+    cin >> num;
+    temp = num;
+
+    while(temp!=0)
+    {
+        rem = temp % 10;
+        sum = sum + rem;
+        temp = temp / 10;
+    }
+    cout << "Sum is: " << sum << endl;
+
+    return 0;
+}
