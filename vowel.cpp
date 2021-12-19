7be6bc4dd2925a40f594a36a7601558f16f990d0
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-19 23:03:58 +0530

    edited

--- vowel.cpp
+++ vowel.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+
 int main()
 {
     char ch;
     cout << "Enter a character: ";
     cin >> ch;
-
     ch = tolower(ch);
 
     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
