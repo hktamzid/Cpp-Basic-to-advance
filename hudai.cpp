e42b730f1fc3ade10efb0f2201ab57b9111c9ee5
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-20 23:38:46 +0530

    nee code

---
+++ hudai.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if(n%2==0)
+    {
+        cout << "Event";
+    }
+    else
+    cout << "Odd";
+
+    return 0;
+}
