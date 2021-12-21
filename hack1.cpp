f90bae8f7bd31bad0723b9821ce3a064c2ac74e5
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-21 22:40:45 +0530

    new code

---
+++ hack1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    scanf("%d%ld%c%f%lf",&a,&b,&c,&d,&e);
+    printf("%d\n",a);
+    printf("%ld\n",b);
+    printf("%c\n",c);
+    printf("%f\n",d);
+    printf("%lf\n",e);
+
+
+    /*cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;*/
+    
+    return 0;
+}
