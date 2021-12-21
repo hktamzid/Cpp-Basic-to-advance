9c6066abdfbb1e65ca71819327a1112915460cb4
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-21 21:04:16 +0530

    new code

---
+++ reverse.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int sum = 0, temp, num, rem;
+    cout << "Enter a number: ";
+    cin >> num;
+    temp = num;
+
+    while(temp!=0)
+    {
+        rem = temp % 10;  
+        sum = sum * 10 + rem;
+        temp = temp / 10;
+    }
+    cout << "The sum of digit is: "<< sum << endl;
+
+    return 0;
+}       
