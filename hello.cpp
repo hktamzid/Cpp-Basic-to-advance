468f29c42198097741a10606ff7d0c2efa8f89d1
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-22 23:42:48 +0530

    new

--- hello.cpp
+++ hello.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int main()
 {
+    int roll;
+    cout << "Enter your roll: ";
+    cin >> roll;
     cout << "Hello world!" << endl;
     cout << "This is Tamzid" << endl;
+    cout << "My roll is " << roll << endl;
 
     return 0;
 }
