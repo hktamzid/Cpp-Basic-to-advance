d65d87d480ed5d749b17f9a7195dff7b13d8d3fe
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-23 21:44:14 +0530

    new

--- pointer_array.cpp
+++ pointer_array.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
     int arr[] = {10,20,30};
-    //cout << &arr[0] << endl;
-    //cout << &arr[1] << endl;
-    //cout << &arr[2] << endl;
+    cout << &arr[0] << endl;
+    cout << &arr[1] << endl;
+    cout << &arr[2] << endl;
 
 
     int *ptr = arr;
