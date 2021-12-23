f3a85bc84f27e831b110c50b7e9d8d9a254aed79
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-23 20:57:08 +0530

    pointer

---
+++ pointer1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int *ptr = &a;
+
+    cout << a << endl;
+    cout << ptr << endl;
+    cout << &a << endl;
+    cout << *ptr << endl;
+
+    return 0;
+}
