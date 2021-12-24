e137e1736dc239856b6645d37e6e0ea536fad496
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-24 21:40:07 +0530

    new program

--- pointer2.cpp
+++ pointer2.cpp
@@ -2,15 +2,18 @@
 #include<conio.h>
 using namespace std;
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main()
 {
     int a = 2;
     int b = 4;
 
-    int temp = a;
-    a = b;
-    b = temp;
-
+    swap(&a,&b);
     cout << a << "  " << b << endl;
 
     return 0;
