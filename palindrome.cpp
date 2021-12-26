70894e89cc41a19386d7b0e2ed868807b62bacfa
hktamzid <20mh1a0536@acoe.edu.in>
2021-12-26 22:46:55 +0530

    new code

---
+++ palindrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int num, sum =0, temp, rem;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    temp = num;
+
+    while(temp!=0)
+    {
+        rem = temp % 10;
+        sum = sum * 10 + rem ;
+        temp = temp / 10;
+    }
+    cout << "Sum of digit: " << sum << endl;
+
+    return 0;
+}
