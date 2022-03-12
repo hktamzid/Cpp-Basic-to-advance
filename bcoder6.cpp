32997595ef99820d3d0481d2639e6011d04d79a2
hktamzid <20mh1a0536@acoe.edu.in>
2022-03-12 11:33:24 +0530

    new code

---
+++ bcoder6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int range = 1;
+    while(range<=15)
+    {
+        cout << 2 << " x " << range << " = " << 2*range << endl;
+        range +=2;
+    }
+
+    return 0;
+}
