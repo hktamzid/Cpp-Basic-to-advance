be89cf52055a99ef835a760ec76994bd2f7890d0
hktamzid <20mh1a0536@acoe.edu.in>
2022-03-12 12:01:12 +0530

    New code

--- bcoder7.cpp
+++ bcoder7.cpp
@@ -8,10 +8,12 @@ int main()
     cin >> n;
     int temp;
 
-    for(int i = 1; i<=n; i ++)
+    for(int i = 1; i*i<=n; i ++)
     {
         if(n%i==0)
         cout << i << " ";
+        if( n / i != i)
+            cout << n / i << " ";
     }
 
     return 0;
---
+++ bcoder8.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0;
+    cout << "Enter any number: ";
+    cin >> n;
+    for (int i=2; i<n; i++)
+    {
+        if(i%n==0)
+        {
+            count++;
+            break;
+        }
+    }
+    if(count==0)
+    {
+        cout << "This is prime";
+    }
+    else
+    {
+        cout << "This is not prime";
+    }
+    return 0;
+}
