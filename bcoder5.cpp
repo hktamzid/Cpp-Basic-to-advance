8dc767aca8b4c73f72d3e9c8c5c0c1c5c6d640bb
hktamzid <20mh1a0536@acoe.edu.in>
2022-03-12 11:24:40 +0530

    new code

---
+++ bcoder5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int table = 2;
+    int range = 15;
+
+    for(range = 1; range<=15; range++)
+    {
+        cout << 2 << " x " << range << " = " << 2*range << endl;
+    }
+
+    return 0;
+}
